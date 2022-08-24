2c95fabe3b902bf0efb19bb194d518b0b9e666f1
Alan <allan.dc@live.com.ar>
2022-08-24 14:52:45 -0300

    'update'

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,6 @@
 import {createStore,applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from "../reducer"; //importo reducer
-
-//1- Creo el store con el reducer. Lo exporto y lo recibe index.js. Allá voy... 
+import rootReducer from "../reducer"; 
 
 export const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
